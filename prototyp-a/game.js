acc6ec3d6889e9b311347429943f710251da1db8
lharrak-botaina <lharrak.botaina@gmail.com>
2021-11-26 06:04:04 -0800

    ADD

--- prototyp-a/game.js
+++ prototyp-a/game.js
@@ -1,14 +1,14 @@
-// function guessgame() {
-//     let randomNr =NMath.floor(Math.random()*101);
-//     let guess;
-//     do {
-//         guess = prompt("devine un nombre entre 1et 100");
-//         console.log("geuss,randomNr");
-//         if (randomNr > guess) {
-//             console.log("trop petit");
-//         } else if (randomNr<guess) {
-//             console.log("trop grand");
-//         }
-//     }while (guess != randomNr);
-// }
-// guessgame();
+function guessgame() {
+    let randomNr =NMath.floor(Math.random()*101);
+    let guess;
+    do {
+        guess = prompt("devine un nombre entre 1et 100");
+        console.log("geuss,randomNr");
+        if (randomNr > guess) {
+            console.log("trop petit");
+        } else if (randomNr<guess) {
+            console.log("trop grand");
+        }
+    }while (guess != randomNr);
+}
+guessgame();
