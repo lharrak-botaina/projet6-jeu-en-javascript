154bc58b921a9db6203a13b68a52f2496986c847
lharrak-botaina <lharrak.botaina@gmail.com>
2021-11-26 07:52:14 -0800

    update

--- prototyp-a/game.js
+++ prototyp-a/game.js
@@ -1,14 +1,20 @@
-function guessgame() {
-    let randomNr =NMath.floor(Math.random()*101);
-    let guess;
-    do {
-        guess = prompt("devine un nombre entre 1et 100");
-        console.log("geuss,randomNr");
-        if (randomNr > guess) {
-            console.log("trop petit");
-        } else if (randomNr<guess) {
-            console.log("trop grand");
-        }
-    }while (guess != randomNr);
+var randomNumber =Math.floor(Math.random()*101);
+
+var userInput = prompt("deviner un nombre entre 1 et 100");
+var winMessage = "Félicitations, vous avez gagné après";
+var lostMessage = "c'est rate !!";
+
+var itisSmall = "c'est plus petit que " + userInput + ",essayez"
+var itisBigger = "c'est plus grand que " + userInput + ",essayez"
+
+function isCorrect() {
+    if (userInput==randomNumber){
+        result = winMessage;
+    } else if (userInput>randomNumber){
+        result = itisSmall
+    }else if (userInput<randomNumber){
+        result = itisBigger
+    }
+    console.log(result)
 }
-guessgame();
+isCorrect()
